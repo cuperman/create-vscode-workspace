8a8f74d162229a55a97c9cd9391cd6f9bb607b82
Jeff Cooper <jeff@cuperman.net>
2020-02-15 06:58:34 -0800

    Change tab spacing to 2

--- bin/create-vscode-workspace.ts
+++ bin/create-vscode-workspace.ts
@@ -8,78 +8,78 @@ const SETTINGS_FILENAME = '.vscode/settings.json';
 const TAB_WIDTH = 4;
 
 const WORKSPACE_TEMPLATE = {
-    folders: [
-        {
-            path: ''
-        }
-    ],
-    settings: {}
+  folders: [
+    {
+      path: ''
+    }
+  ],
+  settings: {}
 };
 
 const ROOT_SETTINGS_TEMPLATE = {
-    'files.exclude': {}
+  'files.exclude': {}
 };
 
 const PROJECT_SETTINGS_TEMPLATE = {};
 
 function workspaceConfig(dir: string, projects: string[] = []) {
-    const folders = [{ path: dir }];
+  const folders = [{ path: dir }];
 
-    projects.forEach(project => {
-        folders.push({
-            path: path.join(dir, project)
-        });
+  projects.forEach(project => {
+    folders.push({
+      path: path.join(dir, project)
     });
+  });
 
-    return Object.assign({}, WORKSPACE_TEMPLATE, { folders });
+  return Object.assign({}, WORKSPACE_TEMPLATE, { folders });
 }
 
 function rootSettingsConfig(projects: string[]) {
-    const excludes = projects.reduce(
-        (excludes: { [subdirectory: string]: boolean }, project) => {
-            return Object.assign({}, excludes, { [project]: true });
-        },
-        {}
-    );
+  const excludes = projects.reduce(
+    (excludes: { [subdirectory: string]: boolean }, project) => {
+      return Object.assign({}, excludes, { [project]: true });
+    },
+    {}
+  );
 
-    return Object.assign({}, ROOT_SETTINGS_TEMPLATE, {
-        'files.exclude': excludes
-    });
+  return Object.assign({}, ROOT_SETTINGS_TEMPLATE, {
+    'files.exclude': excludes
+  });
 }
 
 function projectSettingsConfig(project: string) {
-    return Object.assign({}, PROJECT_SETTINGS_TEMPLATE);
+  return Object.assign({}, PROJECT_SETTINGS_TEMPLATE);
 }
 
 function createJsonFile(file: string, contents: any) {
-    const dirname = path.dirname(file);
+  const dirname = path.dirname(file);
 
-    if (!fs.existsSync(dirname)) {
-        fs.mkdirSync(dirname, { recursive: true });
-    }
-    fs.writeFileSync(file, JSON.stringify(contents, null, TAB_WIDTH));
+  if (!fs.existsSync(dirname)) {
+    fs.mkdirSync(dirname, { recursive: true });
+  }
+  fs.writeFileSync(file, JSON.stringify(contents, null, TAB_WIDTH));
 }
 
 function run() {
-    const cwd = process.cwd();
-    const projects = process.argv.slice(2);
+  const cwd = process.cwd();
+  const projects = process.argv.slice(2);
 
+  createJsonFile(
+    path.join(cwd, WORKSPACE_FILENAME),
+    workspaceConfig(cwd, projects)
+  );
+
+  createJsonFile(
+    path.join(cwd, SETTINGS_FILENAME),
+    rootSettingsConfig(projects)
+  );
+
+  projects.forEach(project => {
     createJsonFile(
-        path.join(cwd, WORKSPACE_FILENAME),
-        workspaceConfig(cwd, projects)
+      path.join(cwd, project, SETTINGS_FILENAME),
+      projectSettingsConfig(project)
     );
-
-    createJsonFile(
-        path.join(cwd, SETTINGS_FILENAME),
-        rootSettingsConfig(projects)
-    );
-
-    projects.forEach(project => {
-        createJsonFile(
-            path.join(cwd, project, SETTINGS_FILENAME),
-            projectSettingsConfig(project)
-        );
-    });
+  });
 }
 
 run();
